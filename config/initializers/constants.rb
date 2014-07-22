996d7cbd3cde3f41681988f5392f5d62357042e1
antonioricardojr <antonioricardojr@gmail.com>
2014-07-22 10:56:37 +0100

    rails app using koala auth

---
+++ config/initializers/constants.rb
@@ -0,0 +1,3 @@
+APP_ID= '309428675901394'
+APP_SECRET= 'bb93a431aa2c57f02f93b6a3a0d7c38b'
+SITE_URL = 'http://giftcheaters.herokuapp.com/'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorApp::Application.config.secret_token = 'e2c2b89e16a24802b7fc642b6a4fae16828a980e6391a0a93c418335c4eabbee5b948cef661bf97d5ebf3db2f4578e1564a32a44204332d09748f8b99a9c6e24'
